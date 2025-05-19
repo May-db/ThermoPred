d523eb4093a6787e7ea5270550753cf9236b8f8b
mariak2707 <maria.kyramarios@epfl.ch>
2025-05-19 18:56:01 +0200

    i commit

---
+++ src/ThermoPred/__init__.py
@@ -0,0 +1,15 @@
+# src/ThermoPred/__init__.py
+
+"""
+ThermoPred - A tool to predict thermodynamic feasibility of chemical reactions
+"""
+
+# You can optionally expose key modules or functions for easier access
+from . import reaction_utils
+
+# Define what gets imported with "from ThermoPred import *"
+__all__ = ['reaction_utils']
+
+# Package metadata
+__version__ = '0.1.0'
+__author__ = 'Maria'
